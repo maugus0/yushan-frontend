ce09b652a501f08a255b3faac9b5212095f8aad8
Ada <499918626@qq.com>
2025-09-17 15:36:10 +0800

    feat: add static login & register pages with reusable AuthForm

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'antd/dist/reset.css';   // Ant Design v5 recommends reset
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
