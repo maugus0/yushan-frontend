85b63a85688685aaffcb7afac7509efa5b36f52b
maugus0 <jaiswalahan987@gmail.com>
2025-09-19 12:22:14 +0800

    Fix ESLint.

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,6 @@
+/* eslint-env node */
+/* global global */
+
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
